7a88497ed5cf51c6b439fc72eccd3d7ca4a1fc54
Clifforid <cliffzhang422@gmail.com>
2023-06-26 21:57:29 +0800

    ✨ feat: xiugai

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:Clifforid/Blogs.git master:gh-pages
+git push -f git@github.com:Clifforid/myBlog.git master:gh-pages
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
